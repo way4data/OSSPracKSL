5b5ab26a32710281294aae7a9e1e9e5482aa648a
KilsupLee <gislee@dgu.ac.kr>
2024-04-01 13:28:05 +0900

    Merge test

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ name='gdhong'
 print(f'I\'m GD Hong!')  
 programming_language='Python'  
 print('Most used language is {0}'.format(programming_language))
-print("checked by KSL")
+print("checked by KSL~")
