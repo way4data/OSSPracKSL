2f7173beaef1926dafc8e2b284cff081712a5664
Kilsup Lee <gislee@dgu.ac.kr>
2024-04-01 16:45:42 +0900

    KSL kslee

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hello Kilsup Lee!")
-print("Hello World!")
+print('hello world')  
+name='gdhong'  
+print(f'I\'m GD Hong!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
+
+
