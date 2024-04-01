af793c59e1da4aa72100e9867cd07958974f9b6f
KilsupLee <gislee@dgu.ac.kr>
2024-04-01 13:43:39 +0900

    M hello k

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('hello world~~~')  
+print('hello world')  
+name='gdhong'  
+print(f'I\'m GD Hong!')  
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
+
