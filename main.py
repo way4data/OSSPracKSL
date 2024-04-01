bb7af00b79620f293ac6d93c52048a44be54e400
Kilsup-Lee (이길섭) <gislee@dgu.ac.kr>
2024-04-01 13:00:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-print("Hello World~")
-=======
 print('hello world')  
 name='gdhong'  
 print(f'I\'m GD Hong!')  
 programming_language='Python'  
 print('Most used language is {0}'.format(programming_language))
->>>>>>> kslee
