d7a01a2a9176389329cc5c321fc9d80d55070f89
Kilsup Lee <gislee@dgu.ac.kr>
2024-04-03 15:47:37 +0900

    M main.py - ksl

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ print("Hello Kilsup Lee!")
 print("Hello World!")
 name = input("이름: ")
 print("환영 " + name + "님!")
-## plang = input("programming_language: ")
-## print('Most used language is {0}'.format(plang))
+
