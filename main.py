f30bc40f6646bf37da0ec31d3b79bfa359401f62
KilsupLee <gislee@dgu.ac.kr>
2024-04-03 14:31:44 +0900

    M main.py main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print('hello world')  
 name='gdhong'  
 print(f'I\'m GD Hong!')  
-programming_language='Python'  
-print('Most used language is {0}'.format(programming_language))
-
-
+plang = input("programming_language: ")
+print('Most used language is {0}'.format(plang))
