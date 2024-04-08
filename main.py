10b2f4e4b53f938304c608acff2fb4d3ece92d56
Kilsup-Lee (이길섭) <gislee@dgu.ac.kr>
2024-04-08 14:25:53 +0900

    P001 Merge main.py 02 (#9)
    * M main.py ks
    
    * M main 01

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Hello Kilsup Lee!")
-print("Hello World!")
+print("Testing Git Merging")
 name = input("이름: ")
 print("환영 " + name + "님!")
+
 plang = input("programming_language: ")
 print('Most used language is {0}'.format(plang))
-
